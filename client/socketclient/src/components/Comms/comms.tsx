af3c6cfd18bf6b46f3e6a1b2d605b76d9d12d050
pai12345 <rahulpai123sap@gmail.com>
2022-06-29 00:03:52 +0200

    Added Chat Page

--- client/socketclient/src/components/Comms/comms.tsx
+++ client/socketclient/src/components/Comms/comms.tsx
@@ -1,47 +1,68 @@
-import * as React from 'react';
-import AppBar from '@mui/material/AppBar';
-import Toolbar from '@mui/material/Toolbar';
-import Typography from '@mui/material/Typography';
-import CssBaseline from '@mui/material/CssBaseline';
-import useScrollTrigger from '@mui/material/useScrollTrigger';
-import Container from '@mui/material/Container';
-import Slide from '@mui/material/Slide';
+import { Fragment } from "react";
+import AppBar from "@mui/material/AppBar";
+import CssBaseline from "@mui/material/CssBaseline";
+import Toolbar from "@mui/material/Toolbar";
+import Typography from "@mui/material/Typography";
+import IconButton from "@mui/material/IconButton";
+import List from "@mui/material/List";
+import ListItem from "@mui/material/ListItem";
+import ListItemAvatar from "@mui/material/ListItemAvatar";
+import ListItemText from "@mui/material/ListItemText";
+import Avatar from "@mui/material/Avatar";
+import MeetingRoomIcon from "@mui/icons-material/MeetingRoom";
+import CommentIcon from "@mui/icons-material/Comment";
+import Box from '@mui/material/Box';
 
-interface Props {
-  window?: () => Window;
-  children: React.ReactElement;
-}
+const messages = [
+  {
+    id: 1,
+    primary: "Brunch this week?",
+    person: "/static/images/avatar/5.jpg",
+  },
+  {
+    id: 2,
+    primary: "Birthday Gift",
+    person: "/static/images/avatar/1.jpg",
+  },
+];
 
-const HideOnScroll = (props: Props) => {
-  const { children, window } = props;
-  const trigger = useScrollTrigger({
-    target: window ? window() : undefined,
-});
-return (
-    <Slide appear={false} direction="down" in={!trigger}>
-      {children}
-    </Slide>
-  );
-}
-
-const comms = (props: Props) => {
+const comms = () => {
   return (
-    <React.Fragment>
+    <Fragment>
       <CssBaseline />
-      <HideOnScroll {...props}>
-        <AppBar>
-          <Toolbar>
-            <Typography variant="h6" component="div">
-              Socket Comms 
-            </Typography>
-          </Toolbar>
-        </AppBar>
-      </HideOnScroll>
-      <Toolbar />
-      <Container>
-        Hello World
-      </Container>
-    </React.Fragment>
+      <Box component="main" sx={{ p: 3 }}>
+      <Typography
+        variant="h5"
+        gutterBottom
+        component="div"
+        sx={{ p: 2, pb: 0 }}
+      >
+        Chat
+      </Typography>
+      <List sx={{ mb: 2 }}>
+        {messages.map(({ id, primary, person }) => (
+          <Fragment key={id}>
+            <ListItem button>
+              <ListItemAvatar>
+                <Avatar alt="Profile Picture" src={person} />
+              </ListItemAvatar>
+              <ListItemText primary={primary} />
+            </ListItem>
+          </Fragment>
+        ))}
+      </List>
+      </Box>
+      <AppBar position="fixed" color="primary" sx={{ top: "auto", bottom: 0 }}>
+        <Toolbar>
+          <IconButton edge="start" color="inherit" aria-label="menu" sx={{ mr: 2 }}>
+            <CommentIcon />
+          </IconButton>
+          <IconButton color="inherit">
+            <MeetingRoomIcon />
+          </IconButton>
+        </Toolbar>
+      </AppBar>
+    </Fragment>
   );
 }
 
--- client/socketclient/src/components/Notfound/notfound.tsx
+++ client/socketclient/src/components/Notfound/notfound.tsx
@@ -1,60 +1,11 @@
-import { Fragment, lazy, Suspense } from 'react';
-import AppBar from '@mui/material/AppBar';
-import Toolbar from '@mui/material/Toolbar';
-import Typography from '@mui/material/Typography';
-import CssBaseline from '@mui/material/CssBaseline';
-import useScrollTrigger from '@mui/material/useScrollTrigger';
-import Container from '@mui/material/Container';
-import Slide from '@mui/material/Slide';
+import { Fragment } from "react";
 
-const NoSsr = lazy(() => import("@mui/material/NoSsr"));
-
-interface Props {
-  window?: () => Window;
-  children: React.ReactElement;
-}
-
-const HideOnScroll = (props: Props) => {
-  const { children, window } = props;
-  const trigger = useScrollTrigger({
-    target: window ? window() : undefined,
-});
-return (
-    <Slide appear={false} direction="down" in={!trigger}>
-      {children}
-    </Slide>
-  );
-}
-
-const notfound = (props: Props) => {
+const notfound = () => {
   return (
     <Fragment>
-    <Suspense
-      fallback={
-        <Fragment>
-         Loading ...
-        </Fragment>
-      }
-    >
-      <NoSsr>
-      <CssBaseline />
-      <HideOnScroll {...props}>
-        <AppBar>
-          <Toolbar>
-            <Typography variant="h6" component="div">
-              Socket Comms 
-            </Typography>
-          </Toolbar>
-        </AppBar>
-      </HideOnScroll>
-      <Toolbar />
-      <Container>
-      404 - Not Found
-      </Container>
-      </NoSsr>
-      </Suspense>
+      404 - Page Not Found
     </Fragment>
   );
-}
+};
 
 export default notfound;
